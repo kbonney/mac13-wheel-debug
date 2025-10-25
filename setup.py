b63f1d90971314e7970376c29d920ee9a61fb0c3
kbonney <kirkb1998@gmail.com>
2025-10-24 20:07:00 -0400

    dynamic versioning

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 from setuptools import setup, Extension
+import datetime
+
+# Generate a unique version based on timestamp
+timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+version = f"0.0.1.dev{timestamp}"
 
 setup(
     name="hello_cibw",
-    version="0.0.1",
+    version=version,
+    description="Test package for macOS 13 cibuildwheel reproduction",
+    long_description="A minimal C extension to test cibuildwheel wheel tagging on macOS 13",
+    long_description_content_type="text/plain",
     ext_modules=[Extension("hello_cibw", ["src/hello.c"])],
 )
